61238f2037dd30a12ae4e7442c9377aeda5c0d10
iSupay <arath_garu22@hotmail.com>
2026-07-15 14:13:07 -0600

    fix: corregir URL de Supabase

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://jkqckwzyfhkycgtsilku.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprcWNrd3p5ZmhreWNndHNpbGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MDk1ODgsImV4cCI6MjA5ODM4NTU4OH0.fnWTmaMFx-Eq7QaPxFtXltIEwgjuka1VpU_Y5hvGDaA';
+const supabaseUrl = 'https://jkqckwzyfnkycgtsilku.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprcWNrd3p5Zm5reWNndHNpbGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNTgwMzIsImV4cCI6MjA1ODczNDAzMn0.fnWTmaMFx-Eq7QaPxFtX1tIEwgjuka1VpU_Y5hvGDaA';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
